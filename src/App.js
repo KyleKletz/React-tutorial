2388fab0dfb23c7c45746d12f248a2350715d37a
Kyle <shadeslayerkyle@yahoo.com>
2020-04-05 18:00:17 -0400

    cleanup master for Clean branch creations

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Game from './Components/Game/Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Game name ="Counting">I am the GAME of </Game>
       </header>
     </div>
   );
