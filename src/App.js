339f24d43f48fa4dafcc97a67af261deb2e6f516
Kyle <shadeslayerkyle@yahoo.com>
2020-04-04 22:53:55 -0400

    Extract logic from return statement

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import ValidationComponent from './Components/ValidationComponent'
 import CharComponent from './Components/CharComponent/CharComponent'
 
 function App() {
-  const [text,updateText] = useState("")
+  const [text,updateText] = useState('')
 
   let updateList = ( index) => {
     let list = [...text]
     list.splice(index,1)
     updateText(list.join(""))
-
-
   }
 
-  function ConvertStringToList(string){
-    return [...string]
-  }
+ let charList = (
+  [...text].map((char,index) =>
+    <CharComponent onClick ={() => updateList(index)} character = {char}></CharComponent>
+    )
+ )
 
   return (
     <div className = "App">
@@ -24,14 +24,7 @@ function App() {
         <input onChange = {(event) => updateText(event.target.value)} type = "text" value = {text}></input>
         <p>{text.length}</p>
         <ValidationComponent length = {text.length}></ValidationComponent>
-        
-        {ConvertStringToList(text).map((char,index) =>
-         
-            <CharComponent onClick ={() => updateList(index)} character = {char}></CharComponent>
-
-          
-        )}
-
+        {charList}
       </header>
     </div>
      
